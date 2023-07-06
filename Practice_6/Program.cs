02fa627837f6655bfe486b10735ffd96051961d6
Herman <barishev_german@mail.ru>
2023-07-07 03:49:57 +0700

    Practice_6

---
+++ Practice_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Кто там?");
+string username = Console.ReadLine();
+if (username.ToLower() == "герман")
+{
+    Console.WriteLine("О, Герман!");
+
+}
+else
+{
+    Console.WriteLine("Привет,я тебе не знаю,");
+    Console.WriteLine(username);
+}
