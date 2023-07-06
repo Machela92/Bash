685296c8f65bc64ad6fb5e6693855889f18ac321
Herman <barishev_german@mail.ru>
2023-07-07 01:14:22 +0700

    Practice_2

---
+++ Practice_2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите ваше Имя");
+string Username = Console.ReadLine();
+Console.Write("Hello,");
+Console.Write(Username);
