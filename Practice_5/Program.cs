db96b60b7a7d66a8cbddb03aba7f1bad3e0751d7
Herman <barishev_german@mail.ru>
2023-07-07 03:01:45 +0700

    Practice 5 + new commands

---
+++ Practice_5/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = new Random().Next(1, 10);
+int b = new Random().Next(1, 10);
+int result = a + b;
+Console.WriteLine(result);
