3f69e59ada9092e5974c879018a422c75f86898a
Herman <barishev_german@mail.ru>
2023-07-08 01:55:59 +0700

    Seminar_homework_1

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Написать программу где принимаются два числа и проверяется является ли второе число квадратом первого
+
+
+
+Console.WriteLine("Для сравнение чисел введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ваше первое число" + " " + a);
+Console.WriteLine("Для сравнение чисел введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ваше второе число" + " " + b);
+int max = a;
+if (a > max) max = a;
+
+else if
+    (b > max) max = b;
+Console.WriteLine("Максимальное число " + max);
+
+
+
