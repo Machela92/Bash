0ac3cc35ea5817314c4b01bba6df294a556219ef
Herman <barishev_german@mail.ru>
2023-07-08 00:50:12 +0700

    Practice_8

---
+++ Practice_8/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+int xa = 1, ya = 1,
+xb = 1, yb = 30,
+xc = 30, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
