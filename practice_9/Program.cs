d4687b6d93d9f4d7416b140c01fb7214b92ae700
Herman <barishev_german@mail.ru>
2023-07-17 03:48:56 +0700

    Practice_9

---
+++ practice_9/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n]; // Здесь мы прописываем длину массива
+for (int i = 0; i < array.Length; i++) // Пробигаемся по миссиву 
+    array[i] = new Random().Next(-10, 11); // Рандомно берет от - 10 до 10
+Console.WriteLine($"{String.Join(",", array)}");
