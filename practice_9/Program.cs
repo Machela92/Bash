7ba7c860cbeffe947b9d04b6533040c8c4c13f28
Herman <barishev_german@mail.ru>
2023-07-19 04:38:11 +0700

    Закончил семинар

--- practice_9/Program.cs
+++ practice_9/Program.cs
@@ -20,7 +20,7 @@ int b = new Random().Next(10, 100);// Генерирует цифру от с 10
 Console.WriteLine(b);
 Console.WriteLine(f(b));
 
-
+// Принимает рандомно 3х значное число и вывдит только первое и  последнее 
 int c = new Random().Next(10, 1000); //10 999
 Console.WriteLine(c);
 int f2(int number)
@@ -28,7 +28,41 @@ int f2(int number)
     int c1 = c / 100;
     int c3 = c % 10;
     return
-      c1 * 10 + c3;
+      c1 * 10 + c3; // Так мы обходим 2 число 
 }
-
 Console.WriteLine(f2(c));
+// Определяем кратно ли первое число второму типа делаиться без остатка =) 
+
+Console.WriteLine("Введите первое число!");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ваше первое число" + " " + first);
+Console.WriteLine("Введите второе число");
+int second = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ваше второе число" + " " + second);
+int result = first % second;
+if (result == 0)
+
+    Console.WriteLine("Делиться без остатка!" + "" + result);
+
+
+else Console.WriteLine("Есть остатоК!" + " " + result);
+
+Console.WriteLine("Проверяем кратное ли ваше число 7 и 23, введите его");
+int sample = Convert.ToInt32(Console.ReadLine());
+if (sample % 7 == 0 && sample % 23 == 0)
+    Console.WriteLine("Делиться");
+else Console.WriteLine("Есть остаток");
+
+
+// Проверяем является ли число квадратом другого числа. 
+Console.WriteLine("Проеверяем явдяется ли число квадротом другого числа");
+Console.WriteLine("Введите первое число!");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ваше первое число" + " " + num1);
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ваше второе число" + " " + num2);
+if (num1 * num1 == num2 || num2 * num2 == num1)
+
+    Console.WriteLine("является");
+else Console.WriteLine("не является");
