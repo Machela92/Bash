b43e1b25b8009f54d3e34e116c019b6fa5ced443
Herman <barishev_german@mail.ru>
2023-07-19 05:44:49 +0700

    Home_work_2

---
+++ Home_work2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int weekday = new Random().Next(1, 6);
+Console.WriteLine(weekday);
+if (weekday < 6)
+    Console.WriteLine("Опять, работать?");
+else
+    Console.WriteLine("Ну все пора отсыпаться, выходные!");
