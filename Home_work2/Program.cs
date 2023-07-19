44372cb841a8b332af5678f81839b34584616f19
Herman <barishev_german@mail.ru>
2023-07-19 22:53:07 +0700

    Задача на поиск второго чилса

--- Home_work2/Program.cs
+++ Home_work2/Program.cs
@@ -1,7 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int weekday = new Random().Next(1, 6);
+/*int weekday = new Random().Next(1, 6);
 Console.WriteLine(weekday);
 if (weekday < 6)
     Console.WriteLine("Опять, работать?");
 else
     Console.WriteLine("Ну все пора отсыпаться, выходные!");
+
+*/
+// Поиск второго числа и проверка на трехзначность, повторение вывод функции 
+Console.WriteLine("Введите трехзначное число");
+int sample_number = Convert.ToInt32(Console.ReadLine());
+bool checking_number(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Вы ввели не трехзначное число");
+        return false;
+    }
+    return true;
+}
+
+int look_for_third_number(int num)
+{
+    int third = num / 10 % 10;
+    return third;
+}
+if (checking_number(sample_number))
+    Console.WriteLine(look_for_third_number(sample_number));
