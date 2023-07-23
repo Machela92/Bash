3e28a35bdf54ac2784e0fefeef37bcea2b60145d
Herman <barishev_german@mail.ru>
2023-07-23 19:51:33 +0700

    Семинар 5

---
+++ Seminar_5/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+void setArray(int[] array) // функция для заполнения  массива 
+{
+    for (int i = 0; i < array.Length; i++) // пробежка по массиву 
+
+        array[i] = new Random().Next(-9, 10);  // При пробежке массива заполняем его случайными числами 
+
+}
+
+Console.WriteLine("Введите  элементы в массив");
+int num = Convert.ToInt32(Console.ReadLine()); // вводим число 
+int[] arr = new int[num]; // Это число  передает какой  длины будет массив
+setArray(arr); // вызов функции 
+Console.WriteLine($"[{string.Join(", ", arr)}]"); // вывод массива на  экран 
