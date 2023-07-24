98308bded0f62a71ca1a4f75db9858a3029498e3
Herman <barishev_german@mail.ru>
2023-07-25 04:40:32 +0700

    Любимая задача

--- Home_work_5/Program.cs
+++ Home_work_5/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-void array_input(int[] array)
+/*void array_input(int[] array)
 {
 
     for (int i = 0; i < array.Length; i++)
@@ -32,7 +32,16 @@ string even_catcher(int[] array) // Метод превращает нечетн
     return $"{x}";
 }
 
-
+string even_catcher1(int[] array, int length)
+{
+    int[] newArray = new int[length];
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 1)
+        {
+            array[i] = newArray[i];
+        }
+    return $"{newArray}";
+}
 Console.WriteLine("Введите кол-во элементов в массиве");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] arr = new int[n];
@@ -41,3 +50,34 @@ Console.WriteLine($"[{string.Join(",", arr)}]");
 Console.WriteLine($"В массиве находятся {even_looker(arr)} четных числа");
 even_catcher(arr);
 Console.WriteLine($"[{string.Join(",", arr)}]");
+even_catcher1(arr, even_looker(arr));
+Console.WriteLine($"[{string.Join(",", arr)}]");
+*/
+void oddArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Math.Round(new Random().NextDouble() * 100, 2);
+}
+
+double finder(double[] array)
+{
+    int max = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > array[max])
+        {
+            array[max] = array[i];
+        }
+    }
+    return array[max];
+}
+Console.WriteLine("Введите кол-во элементов в массиве");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] arr = new double[n];
+oddArray(arr);
+Console.WriteLine($"[{string.Join(", ", arr)}]");
+finder(arr);
+Console.WriteLine(finder(arr));
+
+
+
