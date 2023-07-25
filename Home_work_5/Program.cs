642b6abe8e9716dc2c844dfc3549d208209acf7d
Herman <barishev_german@mail.ru>
2023-07-25 23:11:11 +0700

    Дз 5 готова

--- Home_work_5/Program.cs
+++ Home_work_5/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
 
-/*void array_input(int[] array)
+void array_input(int[] array)
 {
 
     for (int i = 0; i < array.Length; i++)
@@ -44,14 +47,19 @@ string even_catcher1(int[] array, int length)
 }
 Console.WriteLine("Введите кол-во элементов в массиве");
 int n = Convert.ToInt32(Console.ReadLine());
-int[] arr = new int[n];
-array_input(arr);
-Console.WriteLine($"[{string.Join(",", arr)}]");
-Console.WriteLine($"В массиве находятся {even_looker(arr)} четных числа");
-even_catcher(arr);
-Console.WriteLine($"[{string.Join(",", arr)}]");
-even_catcher1(arr, even_looker(arr));
-Console.WriteLine($"[{string.Join(",", arr)}]");
+int[] arr1 = new int[n];
+array_input(arr1);
+Console.WriteLine($"[{string.Join(",", arr1)}]");
+Console.WriteLine($"В массиве находятся {even_looker(arr1)} четных числа");
+even_catcher(arr1);
+Console.WriteLine($"[{string.Join(",", arr1)}]");
+even_catcher1(arr1, even_looker(arr1));
+Console.WriteLine($"[{string.Join(",", arr1)}]");
+
+
+/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 */
 void oddArray(double[] array)
 {
@@ -83,14 +91,52 @@ double min_finder(double[] array)
     return array[min];
 }
 Console.WriteLine("Введите кол-во элементов в массиве");
-int n = Convert.ToInt32(Console.ReadLine());
-double[] arr = new double[n];
+int t = Convert.ToInt32(Console.ReadLine());
+double[] arr = new double[t];
 oddArray(arr);
 Console.WriteLine($"[{string.Join(", ", arr)}]");
 finder(arr);
 Console.WriteLine(finder(arr));
 Console.WriteLine(min_finder(arr));
-double total_result = finder(arr) - min_finder(arr);
+double total_result = Math.Round(finder(arr) - min_finder(arr), 2);
 Console.WriteLine($"{finder(arr)} - {min_finder(arr)} = {total_result}");
 
 
+/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+Console.WriteLine("Введите число элементов в  массиве");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array1 = new int[a];
+
+void array_filler(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(10, 1000);
+}
+
+int oddIndexMath(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum = array[i] + sum;
+    }
+    return sum;
+}
+
+
+void oddIndexshow(int[] array)
+{
+
+    for (int i = 1; i < array.Length; i += 2)
+    {
+
+        Console.WriteLine($"нечетный индекс = {i} числа {array[i]}");
+    }
+}
+array_filler(array1);
+Console.WriteLine($"[{string.Join(",", array1)}]");
+oddIndexshow(array1);
+Console.WriteLine($" Сумма  нечетных инедксов  = {oddIndexMath(array1)}");
